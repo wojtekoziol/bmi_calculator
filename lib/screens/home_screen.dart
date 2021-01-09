a0f72552f3d0adc3bf773eb67c311b1a43b6f929
wojtekoziol <wojtek.koziol.pl@gmail.com>
2021-01-09 15:14:32 +0100

    Add code linting options

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
