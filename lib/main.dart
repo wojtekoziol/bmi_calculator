ccc8967bbeddac8b960fdd19cbba0d4dca4e87e2
wojtekoziol <wojtek.koziol.pl@gmail.com>
2021-01-10 15:21:54 +0100

    Title the app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'BMI Calculator',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
